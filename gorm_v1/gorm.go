33759245a6ef62d06fc010da149e63f49e5ea1d4
Zoltán Nyikos <zoltan.nyikos@bitrise.io>
2021-02-08 20:18:56 +0100

    Implement typesafe transactions

--- gorm_v1/gorm.go
+++ gorm_v1/gorm.go
@@ -8,30 +8,89 @@ import (
 )
 
 var gormType = reflect.TypeOf(&gorm.DB{})
+var errType = reflect.TypeOf((*error)(nil)).Elem()
+var magnetType = reflect.TypeOf(&magnet.Magnet{})
 
-type Transaction interface {
-	Transaction(interface{}) error
-}
+type Transaction struct{}
 
-type transaction struct {
-	*magnet.Magnet
-	db *gorm.DB
-}
+var safeTxType = reflect.TypeOf(Transaction{})
 
-func (t *transaction) Transaction(fn interface{}) error {
-	caller := t.NewCaller(fn, gormType)
-	return t.db.Transaction(func(tx *gorm.DB) error {
-		rv, err := caller.Call(tx)
-		if err != nil {
-			return nil
+func safeTxHook(h magnet.Hook, txType reflect.Type) bool {
+	if txType.Kind() == reflect.Func {
+		if txType.NumIn() == 1 && txType.NumOut() == 1 {
+			cbType := txType.In(0)
+			if cbType.Kind() == reflect.Func && txType.Out(0) == errType && cbType.NumOut() == 1 && cbType.Out(0) == errType {
+				var requires []reflect.Type
+				has := false
+
+				for i := 0; i < cbType.NumIn(); i++ {
+					param := cbType.In(i)
+					if param == safeTxType {
+						has = true
+					} else {
+						requires = append(requires, param)
+					}
+				}
+
+				if !has {
+					return false
+				}
+
+				h.ValidateDeps(requires)
+				factoryParams := []reflect.Type{
+					magnetType,
+					gormType,
+				}
+				factoryType := reflect.FuncOf(
+					factoryParams,
+					[]reflect.Type{txType},
+					false,
+				)
+				factory := reflect.MakeFunc(
+					factoryType,
+					func(in []reflect.Value) []reflect.Value {
+						m := in[0].Interface().(*magnet.Magnet)
+						db := in[1].Interface().(*gorm.DB)
+
+						rv := reflect.MakeFunc(
+							txType,
+							func(in []reflect.Value) []reflect.Value {
+								fn := in[0].Interface()
+								caller := m.NewCaller(fn, gormType, safeTxType)
+								rv := db.Transaction(func(tx *gorm.DB) error {
+									rv, err := caller.Call(tx, Transaction{})
+									if err != nil {
+										return err
+									}
+									if err, ok := rv[0].Interface().(error); ok {
+										return err
+									}
+									return nil
+								})
+								if rv == nil {
+									return []reflect.Value{reflect.Zero(errType)}
+								} else {
+									return []reflect.Value{reflect.ValueOf(rv)}
+								}
+							},
+						)
+						return []reflect.Value{rv}
+					},
+				)
+
+				h.RegisterNewType(
+					factoryParams,
+					txType,
+					factory.Interface(),
+				)
+
+				return true
+			}
 		}
-		if err, ok := rv[0].Interface().(error); ok {
-			return err
-		}
-		return nil
-	})
+	}
+	return false
 }
 
-func NewTransaction(parent *magnet.Magnet, db *gorm.DB) Transaction {
-	return &transaction{parent.NewChild(), db}
+func Use(m *magnet.Magnet) {
+	m.RegisterTypeHook(safeTxHook)
 }
--- gorm_v1/gorm_test.go
+++ gorm_v1/gorm_test.go
@@ -1,13 +1,16 @@
 package gorm_v1_test
 
 import (
+	"database/sql"
 	"database/sql/driver"
 	"errors"
+	"reflect"
 	"testing"
 	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/c2fo/testify/require"
+	"github.com/c2fo/testify/suite"
 	"github.com/jinzhu/gorm"
 	"github.com/nyikos-zoltan/magnet"
 	"github.com/nyikos-zoltan/magnet/gorm_v1"
@@ -25,49 +28,60 @@ func (a AnyTime) Match(v driver.Value) bool {
 	return ok
 }
 
-func Test_GormV1_Transaction(t *testing.T) {
-	t.Run("ok - commit", func(t *testing.T) {
-		m := magnet.New()
-		db, mock, err := sqlmock.New()
-		require.NoError(t, err)
+type GormV1Suite struct {
+	suite.Suite
+	magnet *magnet.Magnet
+	gormDB *gorm.DB
+	mock   sqlmock.Sqlmock
+}
 
-		mock.ExpectBegin()
-		mock.ExpectQuery("INSERT INTO \"?some_models\"?").WithArgs(AnyTime{}, AnyTime{}, nil).
-			WillReturnRows(sqlmock.NewRows([]string{"id"}).FromCSVString("1"))
-		mock.ExpectCommit()
+func (s *GormV1Suite) SetupTest() {
+	s.magnet = magnet.New()
+	gorm_v1.Use(s.magnet)
+	var err error
+	var db *sql.DB
+	db, s.mock, err = sqlmock.New()
+	require.NoError(s.T(), err)
 
-		gormDB, err := gorm.Open("postgres", db)
-		require.NoError(t, err)
-		tx := gorm_v1.NewTransaction(m, gormDB)
+	s.mock.ExpectBegin()
+	s.mock.ExpectQuery("INSERT INTO \"?some_models\"?").WithArgs(AnyTime{}, AnyTime{}, nil).
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).FromCSVString("1"))
+	s.mock.ExpectCommit()
 
-		require.NoError(t, tx.Transaction(func(txDB *gorm.DB) error {
+	s.gormDB, err = gorm.Open("postgres", db)
+	s.magnet.Register(func() *gorm.DB { return s.gormDB })
+	require.NoError(s.T(), err)
+}
+
+type TestTx = func(func(gorm_v1.Transaction, *gorm.DB) error) error
+
+var gormDBType = reflect.TypeOf((*gorm.DB)(nil))
+
+func (s *GormV1Suite) TestOkCommit() {
+	tx := s.magnet.NewCaller(func(tx TestTx) error {
+		return tx(func(_ gorm_v1.Transaction, txDB *gorm.DB) error {
 			return txDB.Create(&SomeModel{}).Error
-		}))
-		if err := mock.ExpectationsWereMet(); err != nil {
-			t.Errorf("there were unfulfilled expectations: %s", err)
-		}
-	})
-	t.Run("err - rollback", func(t *testing.T) {
-		m := magnet.New()
-		db, mock, err := sqlmock.New()
-		require.NoError(t, err)
+		})
+	}, gormDBType)
+	rv, err := tx.Call(s.gormDB)
+	require.NoError(s.T(), err)
+	require.Len(s.T(), rv, 1)
+	require.Nil(s.T(), rv[0].Interface())
+}
 
-		mock.ExpectBegin()
-		mock.ExpectQuery("INSERT INTO \"?some_models\"?").WithArgs(AnyTime{}, AnyTime{}, nil).
-			WillReturnRows(sqlmock.NewRows([]string{"id"}).FromCSVString("1"))
-		mock.ExpectRollback()
-
-		gormDB, err := gorm.Open("postgres", db)
-		require.NoError(t, err)
-		tx := gorm_v1.NewTransaction(m, gormDB)
-
-		err = tx.Transaction(func(txDB *gorm.DB) error {
+func (s *GormV1Suite) TestErrRollback() {
+	tx := s.magnet.NewCaller(func(tx TestTx) error {
+		return tx(func(_ gorm_v1.Transaction, txDB *gorm.DB) error {
 			txDB.Create(&SomeModel{})
 			return errors.New("some error")
 		})
-		require.EqualError(t, err, "some error")
-		if err = mock.ExpectationsWereMet(); err != nil {
-			t.Errorf("there were unfulfilled expectations: %s", err)
-		}
-	})
+	}, gormDBType)
+	rv, err := tx.Call(s.gormDB)
+	require.NoError(s.T(), err)
+	require.Len(s.T(), rv, 1)
+	require.Error(s.T(), rv[0].Interface().(error))
+}
+
+func TestGormV1Suite(t *testing.T) {
+	suite.Run(t, new(GormV1Suite))
 }
